1348f0fad918477cfb24a7b08fd4680ef91e10c9
kjfanj <kjfanj@gmail.com>
2019-04-25 13:57:26 -0500

    added buttons for start reset and display timer

--- client/src/components/main/mainComp/Pomodoro.js
+++ client/src/components/main/mainComp/Pomodoro.js
@@ -2,12 +2,9 @@ import React from 'react';
 import Grid from '@material-ui/core/Grid';
 import Typography from '@material-ui/core/Typography';
 import TextField from '@material-ui/core/TextField';
-
 import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
-import Fab from '@material-ui/core/Fab';
-import TimerIcon from '@material-ui/icons/Timer';
-import LinearProgress from '@material-ui/core/LinearProgress';
+import Button from '@material-ui/core/Button';
 
 const styles = theme => ({
   root: {
@@ -18,121 +15,85 @@ const styles = theme => ({
 
 });
 
-const poo = (<div>poo</div>);
-
-
 class Pomodoro extends React.Component {
 
   constructor(props) {
     super(props);
     this.state = {
-      completed: 0,
-      started: false,
-      name: '',
-      updateCount: 0
+      timeRemaining: 1500,
+      // using
+      hour: "00",
+      minute: "00",
+      second: "00",
+      timerStartedBtn: false,
+
     };
+    // for timer function
     this.timer = null;
   }
 
   componentDidMount() {
-    console.log("componentDidMount")
-    if (this.state.started) {
-      this.timer = setInterval(this.progress, 1000);
-    }
+
   }
   componentWillUpdate(nextProps, nextState) {
-    this.state.updateCount += 1
-    console.log("componentWillUpdate " + this.state.updateCount)
+
   }
 
   componentWillUnmount() {
-    clearInterval(this.timer);
-    console.log("componentWillUnmount " + this.state.updateCount)
+
   }
   componentDidUpdate = (prevProps, prevState) => {
-    this.state.updateCount += 1
 
-    console.log("componentDidUpdate " + this.state.updateCount)
-    clearInterval(this.timer);
-    if (this.state.started) {
-      this.timer = setInterval(this.progress, 1000);
-    }
   };
 
-  progress = () => {
-    const { completed } = this.state;
-    if (completed === 100) {
-      // alert("time up")
-      this.setState({ completed: 0 });
-    } else {
-      const diff = 10;
-      this.setState({ completed: Math.min(completed + diff, 100) });
-    }
-  };
-
-  handleTimer = () => {
-    this.setState({ started: !this.state.started })
+  // button
+  handleStartBtn = () => {
+    this.setState({
+      timerStartedBtn: !this.state.timerStartedBtn
+    })
   }
-
-  handleChange = name => event => {
-    this.setState({ [name]: event.target.value });
-  };
-
 
 
   render() {
     const { classes } = this.props;
-
-    // example of storing jsx in a var
-    // const items = this.state.toDoList.map(e => {
-    //   return (
-    //     <li>{e}</li>
-    //   )
-    const test = (<p>poo</p>);
-
     return (
-      // example of using it
-      //   <ul>
-      //   {items}
-      // </ul>
       <React.Fragment>
-        {/* overall */}
+        {/* ROOT - overall */}
         < Grid className={classes.root} container spacing={24} align="center" >
-          {test}
-          {/* Label */}
+          {/* CHILD - Label */}
           < Typography variant="h6" gutterBottom align="center" >
             TIMER
           </Typography >
-          {/* time card */}
+          {/* CHILD - time display */}
           < Grid container spacing={24} >
             <Grid item xs={12} >
-
+              < Typography variant="h1" gutterBottom align="center" >
+                {this.state.hour}:{this.state.minute}:{this.state.second}
+              </Typography >
             </Grid>
           </Grid >
-          {/* Fab Label */}
+          {/* CHILD - start/pause reset buttons area */}
           < Grid container spacing={24} >
-            <Grid item xs={12} >
-              <Fab
-                size="large"
-                color="primary"
-                aria-label="Timer"
-                className={classes.margin}
-                style={{ marginTop: 20 }}
-                onClick={this.handleTimer}
-              >
-                <TimerIcon />
-              </Fab>
+            {/* filler */}
+            <Grid item xs={3} >
+            </Grid>
+            {/* start pause button */}
+            <Grid item xs={3} >
+              <Button variant="contained" color="primary" onClick={this.handleStartBtn}>
+                {this.state.timerStartedBtn ? "Pause" : "Start"}
+              </Button>
+            </Grid>
+            {/* reset button */}
+            <Grid item xs={3} >
+              <Button variant="contained" color="primary" >
+                Reset
+              </Button>
+            </Grid>
+            {/* filler */}
+            <Grid item xs={3} >
             </Grid>
           </Grid >
-          {/* Progress */}
-          < Grid container spacing={24} >
-            <Grid item xs={12}>
-
-              <LinearProgress variant="determinate" value={this.state.completed} style={{ marginTop: 20 }} />
-              <br />
-            </Grid>
-          </Grid >
-          {/* objective field */}
+          {/* CHILD - objective field */}
           < Grid container spacing={24} >
             <Grid item xs={12}>
               <TextField
