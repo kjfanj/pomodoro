1cdf2599f484bf4ad9d35ff42f922819bab67810
kjfanj <kjfanj@gmail.com>
2019-04-28 21:21:38 -0500

    fixed history UI

--- client/src/components/main/mainComp/History.js
+++ client/src/components/main/mainComp/History.js
@@ -1,14 +1,13 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
-import List from '@material-ui/core/List';
-import ListItem from '@material-ui/core/ListItem';
-import ListItemText from '@material-ui/core/ListItemText';
-import ListItemAvatar from '@material-ui/core/ListItemAvatar';
-import Avatar from '@material-ui/core/Avatar';
 import Typography from '@material-ui/core/Typography';
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
+import Grid from '@material-ui/core/Grid';
+import classNames from 'classnames';
 import Divider from '@material-ui/core/Divider';
-import ThumbUpIcon from '@material-ui/icons/ThumbUp';
+import ThumbUpIcon from '@material-ui/icons/ThumbUp'
 import ThumbDownIcon from '@material-ui/icons/ThumbDown';
 
 // imports for redux
@@ -17,11 +16,26 @@ import { getCompletedTask } from '../../../actions/taskAction';
 const styles = theme => ({
   root: {
     width: '100%',
-    maxWidth: 600,
+    maxWidth: 800,
     backgroundColor: theme.palette.background.paper,
   },
   inline: {
     display: 'inline',
+  },
+  cardGrid: {
+    padding: `${theme.spacing.unit * 8}px 0`,
+  },
+  card: {
+    height: '100%',
+    width: '100%',
+    display: 'flex',
+    flexDirection: 'column',
+  },
+  cardMedia: {
+    paddingTop: '56.25%', // 16:9
+  },
+  cardContent: {
+    flexGrow: 1,
   },
 });
 class History extends React.Component {
@@ -32,41 +46,49 @@ class History extends React.Component {
   render() {
     // console.log(this.props.displayTasks)
     const { classes } = this.props;
+    console.log(this.props.displayTasks)
     const completedTaskToDisplay = this.props.displayTasks.map(task => (
-      <ListItem alignItems="flex-start" key={task._id}>
-        <ListItemText
-          primary={"Task: " + task.objective}
-          secondary={
-            <React.Fragment>
-              <Typography component="span" className={classes.inline} color="textPrimary">
-                Notes:
-                    </Typography>
-              {" "}
+
+      <Grid item key={task._id} sm={12} md={12} lg={12} style={{width: '100%'}}>
+        <Card className={classes.card}>
+          <CardContent className={classes.cardContent}>
+            <Typography>
+              {"Task: " + task.objective}
+            </Typography>
+            <Divider />
+            <Typography align="center">
+              {task.completed ? <ThumbUpIcon /> : <ThumbDownIcon />}
+            </Typography>
+            <Typography align="center">
+              {"rating: " + task.rating}
+            </Typography >
+            <Typography align="center">
+              {task.timer + " m"}
+            </Typography>
+            <Typography align="center">
+              {task.date}
+            </Typography>
+            <Divider />
+            <Typography>
               {task.note ? task.note : "None"}
-            </React.Fragment>
-          }
-        />
-        <ListItemText
-          primary={task.completed ? <ThumbUpIcon /> : <ThumbDownIcon />} />
-        <ListItemText
-          primary={task.timer + "m"} />
-        <ListItemText
-          primary={"rating " + task.rating} />
-        <ListItemText
-          primary={"date " + task.rating} />
-      </ListItem>
+            </Typography>
+          </CardContent>
+
+        </Card>
+      </Grid>
     ))
-
-
-
     return (
 
-      <div>
-        <List className={classes.root}>
+      <div className={classNames(classes.layout, classes.cardGrid)}>
+        <Typography variant="h2" align="center" color="textPrimary" gutterBottom>
+          Total Completed: {this.props.displayTasks.length}
+        </Typography>
+        <Grid container spacing={40}>
           {completedTaskToDisplay}
-        </List>
+        </Grid>
       </div >
-    )
+    );
+
   }
 }
 
