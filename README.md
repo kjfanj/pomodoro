b07c5b669ff26c3585111cfe54c59229da7ce791
kjfanj <kjfanj@gmail.com>
2019-04-29 22:39:05 -0500

    final

--- README.md
+++ README.md
@@ -1,3 +1,12 @@
-A Pomodoro app
+Pomodoro app
+
+A full-stack timer app hosting on Heroku using node and express to serve and route for frontend.
+It allows you to save the tasks you have completed and view your progress over time.
+Redux to ensure that all business logics are handled using the controller.
+Mongoose for database schema and mongoDB Atlas for data storing
+react-google-login for google oAUth sign in for react. 
+Axios to send requests from frontend to backend. 
+Frontend built using React with Material-UI.
+
 
 live at: https://tomato-timer-app.herokuapp.com/
--- client/src/components/main/mainComp/About.js
+++ client/src/components/main/mainComp/About.js
@@ -16,6 +16,52 @@ export default class About extends Component {
           This app's repository
         <Button><a href={"https://github.com/kjfanj/pomodoro"}>GITHUB</a></Button>
         </Typography>
+        <Typography variant="h6" gutterBottom>
+          Currently functionalities
+        </Typography>
+        <Typography>
+          - You need to sign in to view what tasks you have completed
+        </Typography>
+        <Typography>
+          - The default history only shows tasks with 25 minute ran
+        </Typography>
+        <Typography>
+          - You need to sign in to be able to save tasks in db
+        </Typography>
+        <Typography>
+          - You cannot switch to another page while the timer is running because JS is single threaded
+        </Typography>
+        <Typography>
+          - You have to sign in everytime you refresh the page
+        </Typography>
+        <Typography variant="h6" gutterBottom>
+          Maybe in future
+        </Typography>
+        <Typography>
+          - Add web worker to allow you to switch page/tab
+        </Typography>
+        <Typography>
+          - Allow custom time up sound
+        </Typography>
+        <Typography>
+          - Allow custom time
+        </Typography>
+        <Typography>
+          - Use cookie to sign in instead of having to click login everytime
+        </Typography>
+        <Typography>
+          - More filter in History or maybe visualize your history
+        </Typography>
+        <Typography>
+          - different pomodoro session type for you to customize and use
+        </Typography>
+        <Typography variant="h6" gutterBottom>
+          Question?
+        </Typography>
+        <Typography>
+          - send email at tomatotimerapp@gmail.com
+        </Typography>
+
       </div>
     )
   }
